378c710a0b2a4de335b5e59bf381f98769a420c8
Priyansh Pandey <priyanshpandey41@gmail.com>
2024-01-04 22:26:07 +0530

    changed the cors method line

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
 import express from "express";
-import {config} from "dotenv";
-import course from "./routes/courseRoutes.js"
+import { config } from "dotenv";
+import course from "./routes/courseRoutes.js";
 import ErrorMiddleware from "./middlewares/Error.js";
-import  user from "./routes/userRoutes.js";
+import user from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
 import payment from "./routes/paymentroutes.js";
 import other from "./routes/otherRoutes.js";
 import cors from "cors";
 config({
-    path:"./config/config.env"
-})
-const app=express();
+  path: "./config/config.env",
+});
+const app = express();
 
-//using middlewares 
+//using middlewares
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended:true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(cookieParser());
 
 app.use(
-    cors({
-      origin: process.env.FRONTEND_URL,
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-  );
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    //methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 //importing and using routes
 
-app.use("/api/v1",course);
-app.use("/api/v1",user);
-app.use("/api/v1",payment);
-app.use("/api/v1",other);
+app.use("/api/v1", course);
+app.use("/api/v1", user);
+app.use("/api/v1", payment);
+app.use("/api/v1", other);
 export default app;
-
-
 
 app.get("/", (req, res) =>
   res.send(
