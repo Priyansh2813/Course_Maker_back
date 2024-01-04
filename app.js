0afb3f28f6ab81036058b96b96dbe7ec56c0cfcc
Priyansh Pandey <priyanshpandey41@gmail.com>
2024-01-04 23:29:14 +0530

    frontend url added

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://your-frontend.com");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
