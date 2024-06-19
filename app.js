58439bd6917490d88b6ce29603dcc99ac73f80f8
Priyansh Pandey <121034042+Priyansh2813@users.noreply.github.com>
2024-06-20 01:47:34 +0530

    Update app.js cors updated

--- app.js
+++ app.js
@@ -25,7 +25,14 @@ app.use(cookieParser());
 
 
 
-app.use(cors());
+app.use(cors(res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+// Request methods you wish to allow
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// Request headers you wish to allow
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+res.setHeader('Access-Control-Allow-Credentials', true);));
 
 //importing and using routes
 
